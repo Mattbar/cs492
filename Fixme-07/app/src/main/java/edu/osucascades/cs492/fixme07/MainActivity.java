a60b7bac3534fa4ede4cad46da3c71dd8dbeb20b
Mattbar <mlbarnes04@gmail.com>
2019-10-23 09:26:58 -0700

    MainActivity.java Remove: old commented out code and update import to correct Fragment import

--- Fixme-07/app/src/main/java/edu/osucascades/cs492/fixme07/MainActivity.java
+++ Fixme-07/app/src/main/java/edu/osucascades/cs492/fixme07/MainActivity.java
@@ -1,29 +1,8 @@
 package edu.osucascades.cs492.fixme07;
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
-import android.os.Bundle;
 
 public class MainActivity extends /*AppCompatActivity*/ SingleFragmentActivity {
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_fragment);
-//
-//        FragmentManager fm = getSupportFragmentManager();
-//
-//        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-//
-//        if (fragment == null) {
-//            fragment = new CrimeFragment();
-//            fm.beginTransaction()
-//                    .add(R.id.fragment_container, fragment)
-//                    .commit();
-//        }
-//    }
 
     @Override
     protected Fragment createFragment() {
