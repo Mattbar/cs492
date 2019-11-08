522f6be333300a7db1dd5f7e14f88513cdf87d37
Mattbar <mlbarnes04@gmail.com>
2019-11-08 14:23:37 -0800

    add: crimeCursorWrapper class and delete crime method

--- CriminalIntent/app/src/main/java/edu/osucascades/cs492/criminalintent/CrimeListFragment.java
+++ CriminalIntent/app/src/main/java/edu/osucascades/cs492/criminalintent/CrimeListFragment.java
@@ -125,6 +125,7 @@ public class CrimeListFragment extends Fragment {
             mAdapter = new CrimeAdapter(crimes);
             mCrimeRecyclerView.setAdapter(mAdapter);
         } else {
+            mAdapter.setCrimes(crimes);
             mAdapter.notifyItemChanged(mClickedItemPos);
         }
 
@@ -204,6 +205,10 @@ public class CrimeListFragment extends Fragment {
         @Override
         public int getItemCount() {
             return mCrimes.size();
+        }
+
+        public void setCrimes(List<Crime> crimes) {
+            mCrimes = crimes;
         }
 
         @Override
